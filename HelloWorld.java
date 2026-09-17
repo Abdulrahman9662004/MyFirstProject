6561488c172f104cc47b79abe4511eeb18f98609
Abdulrahman9662004 <boodyelkady96@gmail.com>
2026-09-17 08:59:22 +0300

    added new sentence

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,5 @@
-public class HelloWorld { public static void main(String[] args) { System.out.println("Hello, Git & GitHub Branches!"); } }
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello SE411");
+    }
+}
